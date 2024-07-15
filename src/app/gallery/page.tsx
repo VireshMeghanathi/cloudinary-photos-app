51ce0aa955a6cea887a8d16055d2e561c05ca664
VireshMeghanathi <123720255+VireshMeghanathi@users.noreply.github.com>
2024-07-15 21:06:39 +0530

    show images on gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,26 +1,37 @@
-"use client";
-import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
+import { CloudinaryImage } from "./cloudinery-image";
+import Uploadbutton from "./uploadbutton";
+import cloudinary from "cloudinary";
 
-export default function GalleryPage() {
+export default async function GalleryPage() {
+
+    type Searchresult ={
+        public_id: string
+    }
+
+    const results = await cloudinary.v2.search
+        .expression('resource_type:image')
+        .sort_by('created_at', 'desc')
+        .max_results(10)
+        .execute() as {resources: Searchresult[]};
+    // console.log(results);
+
     return <section>
-        <div className="flex justify-between">
+        <div className="flex flex-col gap-8">
+            <div className="flex justify-between">
             <h1 className="text-4xl font-bold">Gallery</h1>
-
-            <Button asChild>
-                <div className="flex gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-                    </svg>
-
-                    <CldUploadButton
-                        onSuccess={(results: UploadResult) => {
-                            // return setImageId(results.info.public_id);
-                        }}
-                        uploadPreset="np9lonvj" />
-                </div>
-            </Button>
+            <Uploadbutton />
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+                {results.resources.map((result)=>(
+                    <CloudinaryImage
+                        key={result.public_id}
+                        src={result.public_id}
+                        width="400"
+                        height="300"
+                        alt="smethin image"
+                    />
+                ))}
+            </div>            
         </div>
     </section>
 }
